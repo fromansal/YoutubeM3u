39a4f396f8d348a782a5174f510aca80cc9077c7
fromansal <91668558+fromansal@users.noreply.github.com>
2024-11-21 12:11:47 +0530

    Update update_playlist.py

--- update_playlist.py
+++ update_playlist.py
@@ -1,18 +1,11 @@
 import os
 import time
-from selenium 
-import webdriver
-from selenium.webdriver.chrome.service 
-import Service
-from selenium.webdriver.common.by 
-import By
-from selenium.webdriver.chrome.options 
-import Options
-from selenium.webdriver.support.ui 
-import WebDriverWait
-from selenium.webdriver.support 
-import expected_conditions 
-as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
